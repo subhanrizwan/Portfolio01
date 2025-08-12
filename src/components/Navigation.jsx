218beda3ac99a7078c0e751d531d4d48818f90ec
subhanrizwan <subhanahmed444sherazi@gmail.com>
2025-08-13 03:56:36 +0500

    portfolio ready on react

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -9,7 +9,7 @@ export default function Navigation() {
     const element = document.getElementById(sectionId);
     if (element) {
       element.scrollIntoView({ behavior: "smooth" });
-      setMobileMenuOpen(false);
+      setMobileMenuOpen(false); // Close menu
     }
   };
 
@@ -18,9 +18,10 @@ export default function Navigation() {
       <button className="w-12 h-12 bg-yellow-400 rounded-full flex items-center justify-center hover:bg-yellow-300 transition-colors">
         <label className="flex flex-col gap-2 w-8">
           <input
-            onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
-            className="peer hidden"
             type="checkbox"
+            className="peer hidden"
+            checked={mobileMenuOpen}
+            onChange={() => setMobileMenuOpen(!mobileMenuOpen)}
           />
           <div className="rounded-2xl h-[3px] w-1/2 bg-black duration-500 peer-checked:rotate-[225deg] origin-right peer-checked:-translate-x-[12px] peer-checked:-translate-y-[1px]" />
           <div className="rounded-2xl h-[3px] w-full bg-black duration-500 peer-checked:-rotate-45" />
@@ -28,15 +29,14 @@ export default function Navigation() {
         </label>
       </button>
 
-      {/*  */}
       {mobileMenuOpen && (
-        <div className="fixed inset-0 bg-black/90 bg-opacity-95 flex items-center justify-center -z-40">
-          <div className="text-center space-y-8 ">
+        <div className="fixed inset-0 bg-black/90 flex items-center justify-center -z-40">
+          <div className="text-center space-y-8">
             {["Home", "About", "Skills", "Portfolio", "Contact"].map((item) => (
               <button
                 key={item}
                 onClick={() => scrollToSection(item.toLowerCase())}
-                className="block text-2xl text-white hover:text-yellow-300 hover:translate-x-5 hover:before:block  hover:before:transition-all hover:before:duration-300 transition duration-300 before:absolute before:-translate-x-4 before:translate-y-[1rem] before:hidden before:w-2 before:bg-yellow-400 before:h-2 before:rounded-full before:content['']"
+                className="block text-2xl text-white hover:text-yellow-300 hover:translate-x-5 transition duration-300 relative before:absolute before:-translate-x-4 before:translate-y-[1rem] before:hidden hover:before:block before:w-2 before:h-2 before:bg-yellow-400 before:rounded-full"
               >
                 {item}
               </button>
