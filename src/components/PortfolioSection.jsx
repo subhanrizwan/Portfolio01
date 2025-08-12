731fd76bb91ceb9089900e8fcd9c4b3e444cfc6e
subhanrizwan <subhanahmed444sherazi@gmail.com>
2025-08-13 03:12:57 +0500

    portfolio ready on react

--- src/components/PortfolioSection.jsx
+++ src/components/PortfolioSection.jsx
@@ -1,5 +1,5 @@
 "use client";
-import proj1 from "../assets/images/first.png";
+import proj1 from "../assets/images/proj1.png";
 import proj2 from "../assets/images/proj2.png";
 import proj3 from "../assets/images/proj3.png";
 import proj4 from "../assets/images/proj4.png";
@@ -17,27 +17,27 @@ export default function PortfolioSection() {
     {
       id: 2,
       image: proj2,
-      link:'https://carlyti.com/'
+      link: "https://carlyti.com/",
     },
     {
       id: 3,
       image: proj3,
-      link:'https://app.netlify.com/projects/subhan-ecommerce-landing/overview'
+      link: "https://app.netlify.com/projects/subhan-ecommerce-landing/overview",
     },
     {
       id: 4,
       image: proj4,
-      link:'https://subhan-task.netlify.app/'
+      link: "https://subhan-task.netlify.app/",
     },
     {
       id: 5,
       image: proj5,
-      link:'https://wonfoods.com/'
+      link: "https://wonfoods.com/",
     },
     {
       id: 6,
       image: proj6,
-      link:'https://subhanahmed.netlify.app/'
+      link: "https://subhanahmed.netlify.app/",
     },
   ];
 
@@ -47,22 +47,20 @@ export default function PortfolioSection() {
       className="py-20 px-6 md:px-12 lg:px-24 bg-gradient-to-b from-black via-yellow-500/10 to-black"
     >
       <div className="max-w-7xl mx-auto">
-                    <Heading Portfolio={'Code Meets Creativity'}/>
+        <Heading Portfolio={"Code Meets Creativity"} />
         {/* Portfolio Grid */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 bg-gr">
           {portfolioItems.map((item) => (
             <div
               key={item.id}
-              target="_blank"
-              onClick={()=> window.open(item.link)}
-              className="group relative overflow-hidden rounded-lg group h-64
-              shadow-lg hover:shadow-xl transition-shadow duration-300"
+              onClick={() => window.open(item.link)}
+              className="group relative overflow-hidden rounded-lg h-64 shadow-[0_2px_40px_rgba(250,204,21,0.1)] transition-shadow duration-300 cursor-pointer"
             >
               <img
                 style={{ transition: "transform 6s linear" }}
                 src={item.image || "/placeholder.svg"}
                 alt={`Portfolio item ${item.id}`}
-                className="w-[100%] h-auto transition-transform duration-300 group-hover:translate-y-[-80%]"
+                className="w-full h-auto absolute top-0 left-0 group-hover:-translate-y-[calc(100%-16rem)] transition-transform"
               />
             </div>
           ))}
